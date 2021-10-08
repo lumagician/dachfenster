c7ec3c07b00f6184cb3614d4690033cb63ee9211
Patrick Althaus <72791209+Snasgul@users.noreply.github.com>
2021-10-08 22:23:29 +0200

    some UI design

--- pages/detailed_info.vue
+++ pages/detailed_info.vue
@@ -1,20 +1,28 @@
 
 <template>
-  <v-container fluid>
+  <v-container fluid grid-list-md>
     <v-row>
-      <v-col class="text-center">
-        <v-card elavation="5">
+      <v-col class="text-left">
+        <v-card elavation="5" style="margin-bottom: 25px">
           <div id="myMap" style="width: 40vw; height: 50vh"></div>
-          <div id='printoutPanel' style="display: none;"></div>
+          <div id="printoutPanel" style="display: none"></div>
         </v-card>
-
         <v-card elavation="5">
-          <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+          <v-card-text>
+            Bin eine Giraffe => brauche ein Dachfenster
+          </v-card-text>
         </v-card>
       </v-col>
-      <v-col class="text-center">
+      <v-col class="text-left">
         <v-card elavation="5">
-          <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+          <v-card-title> La Giraffe </v-card-title>
+          <v-rating
+            half-increments
+            length="5"
+            readonly
+            size="20"
+            value="5"
+          ></v-rating>
         </v-card>
       </v-col>
     </v-row>
@@ -25,56 +33,46 @@
 <script>
 if (process.client) {
   window.GetMap = function GetMap() {
-  var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-    /* No need to set credentials if already passed in URL */
-    center: new Microsoft.Maps.Location(47.606209, -122.332071),
-    zoom: 12,
-  })
-  Microsoft.Maps.loadModule('Microsoft.Maps.Directions', function () {
-    var directionsManager = new Microsoft.Maps.Directions.DirectionsManager(map)
-    // Set Route Mode to driving
-    directionsManager.setRequestOptions({
-      routeMode: Microsoft.Maps.Directions.RouteMode.driving,
+    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+      /* No need to set credentials if already passed in URL */
+      center: new Microsoft.Maps.Location(51.477778, -0.001389),
+      zoom: 16,
     })
-    var waypoint1 = new Microsoft.Maps.Directions.Waypoint({
-      address: 'Driver Start',
-      location: new Microsoft.Maps.Location(
-        46.879114,
-        7.620547
-      ),
+    Microsoft.Maps.loadModule('Microsoft.Maps.Directions', function () {
+      var directionsManager = new Microsoft.Maps.Directions.DirectionsManager(
+        map
+      )
+      // Set Route Mode to driving
+      directionsManager.setRequestOptions({
+        routeMode: Microsoft.Maps.Directions.RouteMode.driving,
+      })
+      var waypoint1 = new Microsoft.Maps.Directions.Waypoint({
+        address: 'Driver Start',
+        location: new Microsoft.Maps.Location(46.879114, 7.620547),
+      })
+      var waypoint2 = new Microsoft.Maps.Directions.Waypoint({
+        address: 'Passenger Start',
+        location: new Microsoft.Maps.Location(46.926458, 7.564239),
+      })
+      var waypoint3 = new Microsoft.Maps.Directions.Waypoint({
+        address: 'Passenger End',
+        location: new Microsoft.Maps.Location(46.947371, 7.438867),
+      })
+      var waypoint4 = new Microsoft.Maps.Directions.Waypoint({
+        address: 'Driver End',
+        location: new Microsoft.Maps.Location(46.925377, 7.416773),
+      })
+      directionsManager.addWaypoint(waypoint1)
+      directionsManager.addWaypoint(waypoint2)
+      directionsManager.addWaypoint(waypoint3)
+      directionsManager.addWaypoint(waypoint4)
+      // Set the element in which the itinerary will be rendered
+      directionsManager.setRenderOptions({
+        itineraryContainer: document.getElementById('printoutPanel'),
+      })
+      directionsManager.calculateDirections()
     })
-    var waypoint2 = new Microsoft.Maps.Directions.Waypoint({
-      address: 'Passenger Start',
-      location: new Microsoft.Maps.Location(
-        46.926458,
-        7.564239
-      ),
-    })
-    var waypoint3 = new Microsoft.Maps.Directions.Waypoint({
-      address: 'Passenger End',
-      location: new Microsoft.Maps.Location(
-        46.947371,
-        7.438867
-      ),
-    })
-    var waypoint4 = new Microsoft.Maps.Directions.Waypoint({
-      address: 'Driver End',
-      location: new Microsoft.Maps.Location(
-        46.925377,
-        7.416773
-      ),
-    })
-    directionsManager.addWaypoint(waypoint1)
-    directionsManager.addWaypoint(waypoint2)
-    directionsManager.addWaypoint(waypoint3)
-    directionsManager.addWaypoint(waypoint4)
-    // Set the element in which the itinerary will be rendered
-    directionsManager.setRenderOptions({
-      itineraryContainer: document.getElementById('printoutPanel'),
-    })
-    directionsManager.calculateDirections()
-  })
-};
+  }
 }
 
 export default {
