4ebd6a6423e571562306c8333b5c3b159fdb1d54
Leo Hofer <33592977+lumagician@users.noreply.github.com>
2021-10-08 22:21:13 +0200

    distance from api

--- api/GetRouteDistance/__init__.py
+++ api/GetRouteDistance/__init__.py
@@ -3,24 +3,20 @@ import logging
 import azure.functions as func
 
 import json
+import requests
 
 url = "http://dev.virtualearth.net/REST/V1/Routes/Driving?o=json&wp.0=Aeugst am Albis&avoid=minimizeTolls&key=AmzCNKlINpVWMJENuTPqdfiR-B5Kg1YM95MtahgZtn-AvzMukcU9GqaZ9Wey9ADd&wp.1=Bonstetten"
-
-x = {
-    "AdressOne" : "Burgdorfstrsse 30, 3510 Konolfingen",
-    "AdressTwo" : "Burgdorfstrsse 30, 3510 Konolfingen",
-    "AdressThree" : "Burgdorfstrsse 30, 3510 Konolfingen",
-    "AdressFour" : "Burgdorfstrsse 30, 3510 Konolfingen"
-}
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    r = request.get(url)
+    r = requests.get(url)
+
+    j = r.json()
 
     return func.HttpResponse(
-            r.json(),
-            status_code=200,
-            mimetype = 'application/json'
+        f"{['resourceSets'][0]['resources'][0]['travelDistance']}",
+        status_code=200,
+        mimetype='application/json'
     )
