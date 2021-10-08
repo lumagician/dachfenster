5821b260f595e8214270be6907588aa3908e1da2
Leo Hofer <33592977+lumagician@users.noreply.github.com>
2021-10-08 21:18:29 +0200

    bing api route test

--- api/GetRouteDistance/__init__.py
+++ api/GetRouteDistance/__init__.py
@@ -4,6 +4,8 @@ import azure.functions as func
 
 import json
 
+url = "http://dev.virtualearth.net/REST/V1/Routes/Driving?o=json&wp.0=Aeugst am Albis&avoid=minimizeTolls&key=AmzCNKlINpVWMJENuTPqdfiR-B5Kg1YM95MtahgZtn-AvzMukcU9GqaZ9Wey9ADd&wp.1=Bonstetten"
+
 x = {
     "AdressOne" : "Burgdorfstrsse 30, 3510 Konolfingen",
     "AdressTwo" : "Burgdorfstrsse 30, 3510 Konolfingen",
@@ -15,7 +17,10 @@ x = {
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    r = request.get(url)
+
     return func.HttpResponse(
-            json.dumps(x),
-            status_code=200
+            r.json(),
+            status_code=200,
+            mimetype = 'application/json'
     )
