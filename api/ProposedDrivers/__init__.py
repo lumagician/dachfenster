c38c8800be96a4edd80488efd9cbe1f5ff44f692
sebug <sebastian.gfeller@gmail.com>
2021-10-08 21:22:33 +0200

    Listing environments

--- api/ProposedDrivers/__init__.py
+++ api/ProposedDrivers/__init__.py
@@ -1,5 +1,6 @@
 import logging
 
+import os
 import azure.functions as func
 import json
 
@@ -7,14 +8,20 @@ import json
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('username')
+    access_key = os.getenv("TABLES_PRIMARY_STORAGE_ACCOUNT_KEY")
+    endpoint_suffix = os.getenv("TABLES_STORAGE_ENDPOINT_SUFFIX")
+    account_name = os.getenv("TABLES_STORAGE_ACCOUNT_NAME")
+    endpoint = "{}.table.{}".format(account_name, endpoint_suffix)
+    connection_string = "DefaultEndpointsProtocol=https;AccountName={};AccountKey={};EndpointSuffix={}".format(
+    account_name, access_key, endpoint_suffix)
+    table_name = "Route"
 
     return func.HttpResponse(json.dumps([
         {
-            'title': 'Func Result 1'
+            'title': 'Func Result 1 ' + access_key
         },
         {
-            'title': 'Func Result 2'
+            'title': 'Func Result 2' + connection_string
         }
     ]), mimetype = 'application/json')
 
