84d7aebd34363d6a3c225035e1549f5312e296b2
sebug <sebastian.gfeller@gmail.com>
2021-10-09 00:42:45 +0200

    Calculate travel duration without upserting

--- api/CalculateTravelDuration/__init__.py
+++ api/CalculateTravelDuration/__init__.py
@@ -6,18 +6,39 @@ import azure.functions as func
 from azure.data.tables import TableClient
 from azure.core.exceptions import HttpResponseError
 from furl import furl
+import requests
 
+BASE_URL = "http://dev.virtualearth.net/REST/V1/Routes/Driving"
+
+def get_duration_with_passenger(driver_start, passenger_start, passenger_end, driver_end):
+    f = furl(BASE_URL)
+    f.args["o"] = "json"
+    f.args["key"] = os.getenv("BING_MAPS_KEY")
+    f.args["optimize"] = "time"
+    f.args["wp.0"] = driver_start
+    f.args["vwp.1"] = passenger_start
+    f.args["vwp.2"] = passenger_end
+    f.args["wp.3"] = driver_end
+    r = requests.get(f.url)
+    j = r.json()
+    return j["resourceSets"][0]["resources"][0]["travelDuration"]
+
+def get_duration_without_passenger(driver_start, driver_end):
+    f = furl(BASE_URL)
+    f.args["o"] = "json"
+    f.args["key"] = os.getenv("BING_MAPS_KEY")
+    f.args["optimize"] = "time"
+    f.args["wp.0"] = driver_start
+    f.args["wp.1"] = driver_end
+    r = requests.get(f.url)
+    j = r.json()
+    return j["resourceSets"][0]["resources"][0]["travelDuration"]
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     driver_name = req.params.get('driver')
     passenger_name = req.params.get('passenger')
-    
-    result = {
-        'driver': driver_name,
-        'passenger': passenger_name
-    }
 
     access_key = os.getenv("TABLES_PRIMARY_STORAGE_ACCOUNT_KEY")
     endpoint_suffix = os.getenv("TABLES_STORAGE_ENDPOINT_SUFFIX")
@@ -33,14 +54,35 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             driver = table_client.get_entity('testing', driver_name)
             passenger = table_client.get_entity('testing', passenger_name)
 
-            logging.info(json.dumps(driver))
-            logging.info(json.dumps(passenger))
-            logging.info(bing_maps_key)
+            driver_start_address = (driver['StartAddressStreet'] + ', ' + driver['StartAddressZipCode'] +
+                ' ' + driver['StartAddressCity'])
+            
+            passenger_start_address = (passenger['StartAddressStreet'] + ', ' + passenger['StartAddressZipCode'] +
+                ' ' + passenger['StartAddressCity'])
+            
+            passenger_destination_address = (passenger['DestinationAddressStreet'] + ', ' + passenger['DestinationAddressZipCode'] +
+                ' ' + passenger['DestinationAddressCity'])
 
-            request_url = furl('http://dev.virtualearth.net/REST/v1/Locations')
-            request_url.args['param1'] = u'vanité'
+            driver_destination_address = (driver['DestinationAddressStreet'] + ', ' + driver['DestinationAddressZipCode'] +
+                ' ' + driver['DestinationAddressCity'])
 
-            logging.info(request_url.url)
+            
+            duration_without_passenger = get_duration_without_passenger(driver_start_address, driver_destination_address)
+
+            duration_with_passenger = get_duration_with_passenger(driver_start_address,
+                passenger_start_address, passenger_destination_address, driver_destination_address)
+
+            logging.info(str(duration_without_passenger) + ' vs ' + str(duration_with_passenger))
+
+            result = {
+                'PartitionKey': 'testing',
+                'RowKey': driver_name + '_' + passenger_name,
+                'Driver': driver_name,
+                'Passenger': passenger_name,
+                'Duration': duration_with_passenger
+            }
+
+
             
             return func.HttpResponse(
                 json.dumps(result),
