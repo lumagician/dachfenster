5486dce5019e056aee05794435fc13ddb1f1a24d
Patrick Althaus <72791209+Snasgul@users.noreply.github.com>
2021-10-08 18:51:36 +0200

    added Bing Maps Key

---
+++ pages/detailed_info.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col class="text-center">
+        <v-card elavation="5">
+          
+        </v-card>
+
+        <v-card elavation="5">
+          <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+        </v-card>
+      </v-col>
+      <v-col class="text-center">
+        <v-card elavation="5">
+          <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+  head: {
+    script: [
+      { hid: 'bingmaps', src: 'http://www.bing.com/api/maps/mapcontrol?callback=GetMap&key=' + process.env.BING_MAPS_KEY, defer: true }
+    ]
+  }
+}
+</script>
