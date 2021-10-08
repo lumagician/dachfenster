948477d493055ebc4e14af7706c71073c310d81f
Leo Hofer <33592977+lumagician@users.noreply.github.com>
2021-10-08 20:52:01 +0200

    dummy function with json response

--- api/GetRouteDistance/__init__.py
+++ api/GetRouteDistance/__init__.py
@@ -2,23 +2,21 @@ import logging
 
 import azure.functions as func
 
+import json
+import requests
+
+x = {
+    "AdressOne" : "Burgdorfstrsse 30, 3510 Konolfingen",
+    "AdressTwo" : "Burgdorfstrsse 30, 3510 Konolfingen",
+    "AdressThree" : "Burgdorfstrsse 30, 3510 Konolfingen",
+    "AdressFour" : "Burgdorfstrsse 30, 3510 Konolfingen"
+}
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse(
+            json.dumps(x),
+            status_code=200
+    )
