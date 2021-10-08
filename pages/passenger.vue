0813f53f52aa4bebe6848c66d5fa93c7ea189994
sebug <sebastian.gfeller@gmail.com>
2021-10-09 00:59:38 +0200

    With subtitle

--- pages/passenger.vue
+++ pages/passenger.vue
@@ -23,6 +23,8 @@
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
+            <v-list-item-subtitle v-text="item.description" />
+            <v-list-item-subtitle v-text="item.goodies" />
           </v-list-item-content>
         </v-list-item>
       </v-list>
