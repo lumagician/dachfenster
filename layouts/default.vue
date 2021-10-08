c53d2710c66cc79cddc9cdc91d3116fecdc6d7af
sebug <sebastian.gfeller@gmail.com>
2021-10-08 15:42:16 +0200

    Light theme

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
@@ -98,7 +98,7 @@ export default {
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Wilkommen',
           to: '/'
         },
         {
