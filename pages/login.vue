29806815068d09dce5d831b8aac756c7e6bc342e
Leo Hofer <33592977+lumagician@users.noreply.github.com>
2021-10-08 16:23:35 +0200

    login only visual

---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-card class="center pa-4" width="400px">
+        <v-card-title justify="center">login</v-card-title>
+        <v-form>
+          <v-text-field label="Email"></v-text-field>
+          <v-text-field label="Password" type="password"></v-text-field>
+          <v-btn>login</v-btn>
+        </v-form>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
