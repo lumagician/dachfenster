89dd6bf748769d5e07416efffcd7cae30172dcdf
sebug <sebastian.gfeller@gmail.com>
2021-10-08 15:19:54 +0200

    Adding Logo

---
+++ pages/passenger.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+	Fahrt Suchen
+        </v-card-title>
+        <v-card-text>
+	<p>Von nach</p>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
