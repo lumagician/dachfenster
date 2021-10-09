6ca9b4114a5a00729789cfe09df41c0b17af4d2d
sebug <sebastian.gfeller@gmail.com>
2021-10-09 06:11:34 +0200

    Putting in spacer to the select button

--- pages/passenger.vue
+++ pages/passenger.vue
@@ -23,7 +23,10 @@
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
-            <v-list-item-subtitle>{{ item.description }}, {{ item.goodies }}
+            <v-list-item-subtitle>
+              
+              {{ item.description }}, {{ item.goodies }}
+              <v-spacer></v-spacer>
                           <v-btn color="primary">Anfragen</v-btn>
             </v-list-item-subtitle>
           </v-list-item-content>
