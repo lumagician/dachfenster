4bf9619c668a7ae2ee80b785c14113474c918397
sebug <sebastian.gfeller@gmail.com>
2021-10-09 07:12:21 +0200

    Passenger request sent

---
+++ pages/passengerRequestSent.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <blockquote class="blockquote">
+        You have successfully requested a ride share with {{ driver }}. We'll let you
+        know once they have made their decision.
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+    setup() {
+        
+    },
+    data() {
+        return {
+            driver: ''
+        };
+    },
+    async fetch() {
+        let driverName = '';
+        if (process.client) {
+            const urlSearchParams = new URLSearchParams(location.search);
+
+            driverName = urlSearchParams.get('driver');
+        }
+        this.driver = driverName;
+    },
+    fetchOnServer: false
+};
+</script>
+
+
